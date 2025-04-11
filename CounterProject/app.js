4a7686410fba24765d3e3c92406f4218dddc4f95
swaraydev <swaray.kmariame@gmail.com>
2025-04-11 21:41:30 +0200

    ajout du projet counterproject

---
+++ CounterProject/app.js
@@ -0,0 +1,31 @@
+// set count
+let count = 0;
+
+// SELECT VALUE AND BUTTONS
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.target.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "#222";
+    }
+    value.textContent = count;
+  });
+});
