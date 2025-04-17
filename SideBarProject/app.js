12a6f0cf8580fc3db2639190afeb44ba6d630507
swaraydev <swaray.kmariame@gmail.com>
2025-04-17 15:54:15 +0200

    Ajout du projet sidebar

---
+++ SideBarProject/app.js
@@ -0,0 +1,19 @@
+// select
+const toggleBtn = document.querySelector(".sideBar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sideBar = document.querySelector(".sideBar");
+
+// click event
+toggleBtn.addEventListener("click", function () {
+  //   console.log(sideBar.classList);
+  //   if (sideBar.classList.contains("show-sidebar")) {
+  //     sideBar.classList.remove("show-sidebar");
+  //   } else {
+  //     sideBar.classList.add("show-sidebar");
+  //   }
+  sideBar.classList.toggle("show-sidebar");
+});
+
+closeBtn.addEventListener("click", function () {
+  sideBar.classList.remove("show-sidebar");
+});
